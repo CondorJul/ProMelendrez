a8028f740da093a21d8b585956aea815ea6d611f
P <ricki.0912@gmail.com>
2022-05-14 12:35:13 -0500

    14/05/2022 c

--- routes/api.php
+++ routes/api.php
@@ -125,7 +125,9 @@ Route::get('v1/appointment-temps/get-all', [AppointmentTempController::class, 'g
 
 
 
-Route::put('v1/appointment-temps/start-call-by-teller', [AppointmentTempController::class, 'startCallByTeller']);
+Route::put('v1/appointment-temps/start-call-by-teller', [AppointmentTempController::class, 'startCallByTeller'])->middleware(['cors']);;
+;
+
 Route::put('v1/appointment-temps/{apptmId}/undo-call', [AppointmentTempController::class, 'undoCall']);
 Route::put('v1/appointment-temps/{apptmId}/call-again', [AppointmentTempController::class, 'callAgain']);
 Route::put('v1/appointment-temps/{apptmId}/finalize-call', [AppointmentTempController::class, 'finalizeCall']);
