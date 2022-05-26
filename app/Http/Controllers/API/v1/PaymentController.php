8691fd4b1df0c71b05fbb4ecbe7e8001bdbb4ad4
P <ricki.0912@gmail.com>
2022-05-26 10:09:14 -0500

    modificado de al 26/05/2022 r

---
+++ app/Http/Controllers/API/v1/PaymentController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade as PDF;
+
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    public function proofOfPayment($token)
+    {
+        $data = [
+            'titulo' => 'Styde.net',
+            'token'=>$token
+        ];
+    
+        return PDF::loadView('accounting.proof-of-payment', $data)
+            ->stream('archivo.pdf');
+    
+        //return $pdf->download('mi-archivo.pdf');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
