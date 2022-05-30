305c7c30bfac843bbc7a867812d197c9df8aa32d
unknown <jc.condor.sistemas@gmail.com>
2022-05-29 21:54:11 -0500

    Actualizado al 29/05/2022 por J

--- resources/views/accounting/proof-of-payment.blade.php
+++ resources/views/accounting/proof-of-payment.blade.php
@@ -1,21 +1,24 @@
-<html>
+<!--<html>
 
 <head>
     <style type="text/css">
         * {
-            background-color: lightblue;
+            /*background-color: #fe0000;*/
             /*width: 300px;*/
             /*margin: 0px 0px 0px 5px;*/
             padding: 0px;
             text-align: center;
+            margin-top: 4px;
         }
 
         table,
         tr,
         th,
         td {
-            border: 1px solid black;
+            /*border: 1px solid black;*/
             border-collapse: collapse;
+            background-color: white;
+            border-radius: 3px;
         }
 
         /*h5,
@@ -35,19 +38,175 @@
     </style>
 </head>
 
-<body style="width: 300px;">
-    <table class="default">
-        <tr>
-            <th rowspan="2" style="width: 196px; font-size: 10px;"><img src="<?php echo $pic ?>" alt="" width="196px"></th>
-            <th style="width: 100px; font-size: 10px;">Cel. 923313696</th>
-        </tr>
-        <tr>
-            <th style="width: 100px;">001-000323</th>
-        </tr>
+<body>
+    <div style="width: 300px; background-color: #fe0000; padding: 5px;">
+        <table class="default" style="width: 300px; border-radius: 3px 3px 3px 3px;">
+            <tr>
+                <th rowspan="2" style="width: 196px; font-size: 10px;"><img style="border-radius: 3px 3px 3px 0px;" src="<?php echo $pic ?>" alt="" width="196px"></th>
+                <th style="width: 100px; font-size: 13px; background-color: white; border-radius: 3px 3px 0px 0px;border-left: 1px solid #fe0000;">20456392815</th>
+            </tr>
+            <tr>
+                <th style="width: 100px; background-color: white; border-radius: 0px 0px 0px 3px; border-top: 1px solid #fe0000; border-left: 1px solid #fe0000;">001-000323</th>
+            </tr>
+        </table>
+        <h5 style="font-size: 7px; font-family: Helvetica; margin-top: 2px; margin-bottom: 5px; background-color: white; border-radius: 0px 0px 3px 3px; font-style: italic; font-weight: lighter;">Av. Daniel Alcides Carrion N° 204(Segundo Nivel) - Urb. San Juan - Yanacancha - Pasco <br> Cel: 973896051 / E-mail: melendres.auditores@hotmail.com</h5>
+        <table class="default" style="width: 300px;">
+            <tr>
+                <th style="width:50px; font-size: 11px; text-align: left; border-bottom: 1px solid #fe0000; font-weight: lighter;">Señor(es):</th>
+                <th colspan="3" style="width:250px; font-size: 11px; text-align: left; border-bottom: 1px solid #fe0000; font-style: italic;">Ricardo Solis Almerco</th>
+            </tr>
+            <tr style="border-bottom: 1px solid #fe0000;">
+                <th style="width:50px; font-size: 11px; text-align: left; font-weight: lighter;">R.U.C:</th>
+                <th style="width:155px; font-size: 11px; text-align: left; font-style: italic;">10724563924</th>
+                <th style="width:40px; font-size: 11px; text-align: left; font-weight: lighter;">Fecha:</th>
+                <th style="width:50px;font-size: 11px; text-align: left; font-style: italic;">29/05/2022</th>
+            </tr>
+            <tr>
+                <th style="width:50px; font-size: 11px; text-align: left; font-weight: lighter;">Direccion:</th>
+                <th style="width:155px; font-size: 11px; text-align: left; font-style: italic;">Av. Jose Martin Columna Pasco</th>
+                <th style="width:40px; font-size: 11px; text-align: left; font-weight: lighter;">Celular:</th>
+                <th style="width:50px;font-size: 11px; text-align: left; font-style: italic;">923313696</th>
+            </tr>
+        </table>
+        <table class="default" style="width: 300px;">
+            <tr>
+                <th style="width:30px; font-size: 11px; text-align: center; border-right: 1px solid #fe0000;">Cant.</th>
+                <th style="width:155px; font-size: 11px; text-align: center; border-right: 1px solid #fe0000;">Descripcion</th>
+                <th style="width:35px; font-size: 11px; text-align: center; border-right: 1px solid #fe0000;">P. Unit.</th>
+                <th style="width:30px;font-size: 11px; text-align: center;">Importe</th>
+            </tr>
+            <tr>
+                <th style="width:30px; font-size: 11px; text-align: center; border-right: 1px solid #fe0000; font-style: italic; font-weight: lighter;">1</th>
+                <th style="width:155px; font-size: 11px; text-align: left; border-right: 1px solid #fe0000; font-style: italic; font-weight: lighter;">Por Declaracion Jurada Anual -2021</th>
+                <th style="width:35px; font-size: 11px; text-align: center; border-right: 1px solid #fe0000; font-style: italic; font-weight: lighter;">50.00</th>
+                <th style="width:30px;font-size: 11px; text-align: right; font-style: italic; font-weight: lighter;">2000.00</th>
+            </tr>
+        </table>
+        <table class="default" style="width: 300px;">
+            <tr>
+                <th style="height: 40px; width:100px; font-size: 11px; text-align: center; border-right: 1px solid #fe0000;">Canjear por Factura o Boleta de Venta</th>
+                <th style="width:112px; font-size: 9px; text-align: center; border-right: 1px solid #fe0000;font-weight: lighter;"><br>..............................................<br>Melendres Auditores</th>
+                <th style="width:40px; font-size: 11px; text-align: center;">Total</th>
+                <th style="font-size: 11px; text-align: right;">2000.00</th>
+            </tr>
+        </table>
 
+    </div>
+</body>
+
+</html>-->
+
+<html>
+
+<head>
+    <style type="text/css">
+        * {
+            /*background-color: #fe0000;*/
+            /*width: 300px;*/
+            /*margin: 0px 0px 0px 5px;*/
+            padding: 0px;
+            text-align: center;
+            margin-top: 4px;
+        }
+
+        table,
+        tr,
+        th,
+        td {
+            /*border: 1px solid black;*/
+            border-collapse: collapse;
+            background-color: white;
+        }
+
+        /*h5,
+        h2 {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        h4 {
+            margin-top: 2px;
+            margin-bottom: 0px;
+        }
+
+        th {
+            padding: 0px;
+        }*/
+    </style>
+</head>
+
+<body style="width: 300px; padding: 5px;">
+    <img src="<?php echo $pic ?>" alt="" width="300px">
+    <h5 style="font-size: 7px; font-family: Helvetica; margin-top: 2px; margin-bottom: 5px; font-style: italic; font-weight: lighter;">Av. Daniel Alcides Carrion N° 204(Segundo Nivel) - Urb. San Juan - Yanacancha - Pasco <br> Cel: 973896051 / E-mail: melendres.auditores@hotmail.com</h5>
+    <div>
+        <th style="width: 100px; border: 2px solid black;">001-000323</th>
+    </div>
+
+    <table>
+        <tr>
+            <th style="width: 300px; font-size: 2px; border-bottom: 1px solid;"></th>
+        </tr>
     </table>
-    <h5 style="font-size: 12px; font-family: Helvetica; margin-top: 8px; font-weight: Italic; margin-bottom: 10px;">
-        Av. Daniel Alcides Carrion N° 204(Segundo Nivel) - Urb. San Juan - Yanacancha - Pasco</h5>
+    <table class="default" style="width: 300px;">
+        <tr>
+            <th style="width:50px; font-size: 11px; text-align: left; font-weight: lighter;">Señor(es):</th>
+            <th colspan="3" style="width:250px; font-size: 11px; text-align: left; font-style: italic;">Ricardo Solis Almerco</th>
+        </tr>
+        <tr>
+            <th style="width:50px; font-size: 11px; text-align: left; font-weight: lighter;">R.U.C:</th>
+            <th style="width:155px; font-size: 11px; text-align: left; font-style: italic;">10724563924</th>
+            <th style="width:40px; font-size: 11px; text-align: left; font-weight: lighter;">Fecha:</th>
+            <th style="width:50px;font-size: 11px; text-align: left; font-style: italic;">29/05/2022</th>
+        </tr>
+        <tr>
+            <th style="width:50px; font-size: 11px; text-align: left; font-weight: lighter;">Direccion:</th>
+            <th style="width:155px; font-size: 11px; text-align: left; font-style: italic;">Av. Jose Martin Columna Pasco</th>
+            <th style="width:40px; font-size: 11px; text-align: left; font-weight: lighter;">Celular:</th>
+            <th style="width:50px;font-size: 11px; text-align: left; font-style: italic;">923313696</th>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <th style="width: 300px; font-size: 2px; border-bottom: 1px solid black;"></th>
+        </tr>
+    </table>
+    <table class="default" style="width: 300px;">
+        <tr>
+            <th colspan="4" style="border-top: 1px dashed black;"></th>
+        </tr>
+        <tr>
+            <th style="width:30px; font-size: 11px; text-align: center;">Cant.</th>
+            <th style="width:155px; font-size: 11px; text-align: center;">Descripcion</th>
+            <th style="width:35px; font-size: 11px; text-align: center;">P. Unit.</th>
+            <th style="width:30px;font-size: 11px; text-align: center;">Importe</th>
+        </tr>
+        <tr>
+            <th colspan="4" style="border-top: 1px dashed black;"></th>
+        </tr>
+        <tr>
+            <th style="width:30px; font-size: 11px; text-align: center; font-style: italic; font-weight: lighter;">1</th>
+            <th style="width:155px; font-size: 11px; text-align: left; font-style: italic; font-weight: lighter;">Por Declaracion Jurada Anual -2021</th>
+            <th style="width:35px; font-size: 11px; text-align: center; font-style: italic; font-weight: lighter;">50.00</th>
+            <th style="width:30px;font-size: 11px; text-align: right; font-style: italic; font-weight: lighter;">2000.00</th>
+        </tr>
+        <tr>
+            <th colspan="4" style="border-top: 1px dashed black;"></th>
+        </tr>
+        <tr>
+            <th colspan="2" style="width:30px; font-size: 11px; text-align: center; font-style: italic; font-weight: lighter;"></th>
+            <th style="width:35px; font-size: 11px; text-align: center; font-style: italic; font-weight: lighter;">Total</th>
+            <th style="width:30px;font-size: 11px; text-align: right; font-style: italic; font-weight: lighter;">2000.00</th>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <th style="width: 300px; font-size: 2px; border-bottom: 1px solid;"></th>
+        </tr>
+    </table>
+    <br>
+    <div style="font-size: 9px; text-align: center; font-weight: lighter;"><br>..............................................<br>Melendres Auditores</div>
+    <div style="font-size: 11px; text-align: center;">Canjear por Factura o Boleta de Venta</div>
+
 
 
 </body>
